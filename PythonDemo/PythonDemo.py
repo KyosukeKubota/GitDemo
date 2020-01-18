efe18ec8fd3f624a1a3b0fa24e807f2f03acbcae
Kyosuke Kubota <ricecake1456@gmail.com>
2020-01-18 16:46:05 +0900

    for分追加に戻す

--- PythonDemo/PythonDemo.py
+++ PythonDemo/PythonDemo.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 p = dt.date.today()
-print(p)
+t = dt.datetime.now()
 
 a = 5 + 8
 b  = 8 / 3
@@ -16,4 +16,4 @@ if a%3 == 0:
 elif a%3 == 1:
     print("a%3 == 1")
 else:
-    print("a%3 == 2")
+    print("a % 2 == 2")
