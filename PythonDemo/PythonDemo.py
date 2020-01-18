a46188ddf7663c332c63f183bf95400cd388da26
Kyosuke Kubota <ricecake1456@gmail.com>
2020-01-18 16:33:23 +0900

    ifelse追加

--- PythonDemo/PythonDemo.py
+++ PythonDemo/PythonDemo.py
@@ -1,13 +1,19 @@
 import datetime as dt
 
 p = dt.date.today()
-print(p)
-
 t = dt.datetime.now()
-print(t)
 
 a = 5 + 8
 b  = 8 / 3
 
+print(p)
+print(t)
 print(a)
 print(b)
+
+if a % 3 == 0:
+    print("a % 3 == 0")
+elif a % 3 == 1:
+    print("a % 3 == 1")
+else:
+    print("a % 2 == 2")
