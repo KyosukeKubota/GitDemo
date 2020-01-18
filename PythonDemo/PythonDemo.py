10cd5135a5f4ffa05e51e99faccc0b95da1e3d8b
Kyosuke Kubota <ricecake1456@gmail.com>
2020-01-18 16:40:37 +0900

    ブランチ

--- PythonDemo/PythonDemo.py
+++ PythonDemo/PythonDemo.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 p = dt.date.today()
-t = dt.datetime.now()
+print(p)
 
 a = 5 + 8
 b  = 8 / 3
@@ -10,16 +10,3 @@ print(p)
 print(t)
 print(a)
 print(b)
-
-if a % 3 == 0:
-    print("a % 3 == 0")
-elif a % 3 == 1:
-    print("a % 3 == 1")
-else:
-    print("a % 2 == 2")
-
-for item in [0,1,2,3,4,5]:
-    print(item)
-
-for item in range(0,6):
-    print(item)
